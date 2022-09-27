c6464fb7a3d92668a5e986ddd3e54de5ed6fb675
Cheyenne Hohman <j.cheyenne.hohman@gmail.com>
2022-09-27 10:36:09 -0400

    JS Basics Exercise

---
+++ script.js
@@ -0,0 +1,54 @@
+console.log(7);
+
+// 5 data types/primitives
+
+/*
+Numbers
+Strings
+Booleans
+Null
+Undefined
+*/
+
+// 6 Operator Symbols
+
+/*
+Addition + 
+Subtraction - 
+Multiplication *
+Division /
+Modulus %
+Exponent ** 
+
+EXTRAS 
+Increment ++
+Decrement -- 
+*/
+
+console.log(5 % 2); //1
+console.log(5 % 4); //1
+console.log('I\'M MARY POPPINS, Y\'ALL!'); //I'M MARY POPPINS, Y'ALL! 
+console.log('Hawk' + 'eye'); //Hawkeye
+console.log('Cheyenne'.length + 'Hohman'.length); //14
+console.log("Avengers: Endgame" [16]); //e
+
+// BONUS STUFF 
+console.log('\\'); //\
+
+console.log(5 ** 4 - 9); //616
+
+console.log("I".length - "Am".length + "Iron".length - "Man".length); //0
+
+console.log("CAP"[1]); //A
+console.log("VISION"[0]); //V
+console.log("OKOYE"[4]); //E
+console.log("NICK FURY"[0]); //N
+console.log("GROOT"[0]); //G
+console.log("DOCTOR STRANGE"[13]); //E
+console.log("THOR"[3]); //R
+console.log("STAR-LORD"[0]); //S
+
+var m = 25;
+var c = 4;
+var u = m + c;
+console.log(u); //29
